b80c544dc00f8b0ee8da3ad61a88fe2a30306c80
chriswebdv <chriswebdv@gmail.com>
2024-11-25 17:56:23 -0600

    Add computer choice function

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-console.log("Hello all!");
-console.log("Testing");
+const choices = ["rock", "paper", "scissors"];
+
+const computerChoice = () => {
+    const randomChoice = Math.floor(Math.random() * 3);
+    return choices[randomChoice];
+}
+console.log(computerChoice()); 
