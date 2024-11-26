e5416613491930abe36b765073f82389f010cc03
chriswebdv <chriswebdv@gmail.com>
2024-11-26 16:27:41 -0600

    Add relevant comments

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
+// create variable which includes the computers choices
 const choices = ["rock", "paper", "scissors"];
+
+// create the variables to keep track of the scores and total games
 let humanScore = 0;
 let computerScore = 0;
 let totalGames = 0;
 
+// function that returns a random computer choice from the array
 const getComputerChoice = () => {
     const randomChoice = Math.floor(Math.random() * 3);
     return choices[randomChoice];
 }
 
+// create the human answer and equal to null to avoid tolowercase and trim methods errors
 let answer = null;
 
 if (answer !== null) {
   answer = answer.toLowerCase().trim(); 
 }
 
+// Loops which contains all of the games and logic 
 while (answer !== "") {
     console.log("what is your choice: ROCK, PAPER or SCISSORS? ");
     answer = prompt();
@@ -54,6 +60,7 @@ while (answer !== "") {
     }
 }
 
+// conditional to check who the winner is
 if (computerScore > humanScore) {
     console.log(`Computer won! with a total of ${computerScore} points in ${totalGames} games!`);
 } else if (computerScore < humanScore) {
