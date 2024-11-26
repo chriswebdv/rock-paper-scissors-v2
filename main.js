b31b59ecd83cd0b5aaaa2b304130167bf61b51c6
chriswebdv <chriswebdv@gmail.com>
2024-11-26 16:21:54 -0600

    Refactor code to get it to work correctly

--- main.js
+++ main.js
@@ -8,70 +8,56 @@ const getComputerChoice = () => {
     return choices[randomChoice];
 }
 
-const getHumanChoice = () => {
-    while (true) {
-        let answer = prompt().toLowerCase().trim();
+let answer = null;
 
-        if (answer === "rock" || answer === "paper" || answer === "scissors") {
-            return answer;
-        } else {
-            continue;
-        }
-    }
-
+if (answer !== null) {
+  answer = answer.toLowerCase().trim(); 
 }
 
-const playGame = (humanChoice, computerChoice) => {
-    while (totalGames <= 5) {
-        humanChoice;
-        if (humanChoice === "") {
-            continue;
-        }
-        if (getHumanChoice() === "rock" && computerChoice === "paper") {
-            console.log("Computer wins!");
-            computerScore++;
-            totalGames++;
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("Human wins!");
-            humanScore++;
-            totalGames++;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("Human wins!");
-            humanScore++;
-            totalGames++;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            console.log("Computer wins!");
-            computerScore++;
-            totalGames++;
-        } else if (humanChoice === "paper" && computerChoice === "paper") {
-            console.log("tied game!");
-            continue;
-        } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-            console.log("tied game!");
-            continue;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            console.log("Computer wins!");
-            computerScore++;
-            totalGames++;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("Human wins!");
-            humanScore++;
-            totalGames++;
-        } else if (humanChoice === "rock" && computerChoice === "rock") {
-            console.log("tied game!");
-            continue;
-        }
-    }
-    if (computerScore > humanScore) {
-        console.log(`Computer won! you have a total of ${computerScore} points in ${totalGames} games!`);
-    } else if (computerScore < humanScore) {
-        console.log(`Human won! you have a total of ${humanScore} points in ${totalGames} games!`);
-    } else {
-        console.log("Tied game!");
+while (answer !== "") {
+    console.log("what is your choice: ROCK, PAPER or SCISSORS? ");
+    answer = prompt();
+
+    if (answer === "rock" && getComputerChoice() === "paper") {
+        //console.log("Computer wins!");
+        computerScore++;
+        totalGames++;
+    } else if (answer === "rock" && getComputerChoice() === "scissors") {
+        //console.log("Human wins!");
+        humanScore++;
+        totalGames++;
+    } else if (answer === "paper" && getComputerChoice() === "rock") {
+        //console.log("Human wins!");
+        humanScore++;
+        totalGames++;
+    } else if (answer === "paper" && getComputerChoice() === "scissors") {
+        //console.log("Computer wins!");
+        computerScore++;
+        totalGames++;
+    } else if (answer === "paper" && getComputerChoice() === "paper") {
+        //console.log("tied game!");
+        totalGames++;
+    } else if (answer === "scissors" && getComputerChoice() === "scissors") {
+        //console.log("tied game!");
+        totalGames++;
+    } else if (answer === "scissors" && getComputerChoice() === "rock") {
+        //console.log("Computer wins!");
+        computerScore++;
+        totalGames++;
+    } else if (answer === "scissors" && getComputerChoice() === "paper") {
+        //console.log("Human wins!");
+        humanScore++;
+        totalGames++;
+    } else if (answer === "rock" && getComputerChoice() === "rock") {
+        //console.log("tied game!");
+        totalGames++;
     }
 }
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-
-playGame(humanSelection, computerSelection);
+if (computerScore > humanScore) {
+    console.log(`Computer won! with a total of ${computerScore} points in ${totalGames} games!`);
+} else if (computerScore < humanScore) {
+    console.log(`Human won! you have a total of ${humanScore} points in ${totalGames} games!`);
+} else {
+    console.log("Tied game!");
+}
