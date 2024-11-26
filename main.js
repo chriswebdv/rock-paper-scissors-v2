a092789563accd07e08b9e45f6ef94af05e32df9
chriswebdv <chriswebdv@gmail.com>
2024-11-25 18:09:45 -0600

    Create new function which capture the user choice if not choice given ask for user input again

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const choices = ["rock", "paper", "scissors"];
 
-const computerChoice = () => {
+const getComputerChoice = () => {
     const randomChoice = Math.floor(Math.random() * 3);
     return choices[randomChoice];
 }
-console.log(computerChoice()); 
+getComputerChoice();
+
+const getHumanChoice = () => {
+    while (true) {
+        let answer = prompt().toLowerCase();
+
+        if (answer === "rock" || answer === "paper" || answer === "scissors") {
+            return answer;
+        } else {
+            continue;
+        }
+    }
+
+}
+console.log(getHumanChoice());
